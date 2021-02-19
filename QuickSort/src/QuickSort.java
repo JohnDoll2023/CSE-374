9b3e7c617fdcde2e1c114211ee172a032cbd6381
John Doll <dolljm@miamioh.edu>
2021-02-19 01:49:07 -0500

    Initial commit

---
+++ QuickSort/src/QuickSort.java
@@ -0,0 +1,13 @@
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QuickSort();
+	}
+	
+	public static void QuickSort() {
+		
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSE-374
+ Algorithms I
