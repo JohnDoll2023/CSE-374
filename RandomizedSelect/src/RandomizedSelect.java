cb07d0604ab251fab85c88cb9924f62df87acbad
dolljm <dolljm@miamioh.edu>
2021-02-25 13:30:21 -0500

    new randomized select

--- RandomizedSelect/src/RandomizedSelect.java
+++ RandomizedSelect/src/RandomizedSelect.java
@@ -1,7 +1,7 @@
 public class RandomizedSelect {
 	public static void main(String[] args) {
 		int arr [] = {8, 3, 5, 9, 21, 6, 5, -1, 20, 0}; 
-		int i = 2;
+		int i = 10;
 		System.out.println(randomizedSelect(arr, 0, arr.length - 1, i));
 		
 	}
@@ -11,22 +11,34 @@ public class RandomizedSelect {
 		if(index == length) {
 			return arr[index];
 		}
-		//helper method
-		int q = randomizedPartition(arr, index, length);
-		//creates point to see if we have our index
-		int k = q - index + 1;
-		//checks if our index equals the location where we partioned
+		//helper method to find pivot
+		int pivot = randomizedPartition(arr, index, length);
+		//how many values in the array are less than itself (k), represents how many elements in left portion including q
+		int k = pivot - index + 1;
+		//checks if our index equals the location where we partitioned
 		if(i == k) {
-			return arr[q];
+			return arr[pivot];
 		//checks if i is less than k, indicating that we want to work with the values less than k in our new randomized select
 		} else if (i < k) {
-			return randomizedSelect(arr, index, q - 1, i);
+			return randomizedSelect(arr, index, pivot - 1, i);
 		} else {
-			return randomizedSelect(arr, q + 1, length, i - k);
+			//shift over i because we need to adjust for not having as many elements k since we split the array
+			return randomizedSelect(arr, pivot + 1, length, i - k);
 		}
 	}
 	
 	public static int randomizedPartition(int arr[], int index, int length) {
+		// gets random location to be pivot
+		int randomLocation = (int)(Math.random()*(length - index + 1) + index);
+		// swaps this with the last value
+		int temp = arr[length];
+		arr[length] = arr[randomLocation];
+		arr[randomLocation] = temp;
+		// partitions 
+		return partition(arr, index, length);
+	}
+	
+	public static int partition(int arr[], int index, int length) {
 		//set a our comparison value that all numbers to the left will be less than
 		int key = arr[length];
 		//location of last value that is lower than our key
